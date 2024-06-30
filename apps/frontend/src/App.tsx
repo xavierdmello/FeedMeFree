959680110b06488f91faedd74c7cd095adac4a8c
Xavier DMello <xaviermdmello@gmail.com>
2024-06-30 07:07:48 -0700

    map

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { DAppKitProvider } from "@vechain/dapp-kit-react";
-import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
+import { ChakraProvider, Container, Flex , Box} from "@chakra-ui/react";
 import {
   Dropzone,
   Footer,
@@ -10,8 +10,27 @@ import {
 } from "./components";
 import { lightTheme } from "./theme";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  mapboxgl.accessToken = 'pk.eyJ1IjoidGFjb2NhdDQ2NDIiLCJhIjoiY2x5MHU3dGliMHNleTJsb2lheTJqeDdnZiJ9.D0_LMnUu36qWkg6pscuK2Q';
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(-70.9);
+  const [lat, setLat] = useState(42.35);
+  const [zoom, setZoom] = useState(9);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [lng, lat],
+      zoom: zoom
+    });
+  });
+
   return (
     <ChakraProvider theme={lightTheme}>
       <DAppKitProvider
@@ -21,11 +40,10 @@ function App() {
         nodeUrl="https://testnet.vechain.org/"
         logLevel={"DEBUG"}
       >
-        <Navbar />
-    
-
-        <Footer />
-
+        <Box h={"500px"}>
+    <Box ref={mapContainer} className="map-container" h={"500px"} />
+        </Box>
+  
         {/* MODALS  */}
         <SubmissionModal />
       </DAppKitProvider>
